94e234adbd488e53f5c54a69ea362be5e5f2f636
Alexander S <alexsvistunov73@gmail.com>
2023-12-21 11:50:05 +0300

    declare user orm model

---
+++ table_user.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class User(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True)
+    age = Column(Integer)
+    city = Column(String)
+    country = Column(String)
+    exp_group = Column(Integer)
+    gender = Column(Integer)
+    os = Column(String)
+    source = Column(String)
