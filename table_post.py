2bad298f64d1fd82889f07f93732d544a2d548a0
Alexander S <alexsvistunov73@gmail.com>
2023-12-21 11:48:55 +0300

    add get post_list query using orm

--- table_post.py
+++ table_post.py
@@ -1,8 +1,25 @@
 from sqlalchemy import Column, Integer, String
-from database import Base
+from database import Base, SessionLocal
 
 class Post(Base):
     __tablename__ = "post"
     id = Column(Integer, primary_key=True)
     text = Column(String)
     topic = Column(String)
+
+
+if __name__ == "__main__":
+    session = SessionLocal()
+
+    post_list = []
+
+    for post in (
+        session.query(Post)
+        .filter(Post.topic == 'business')
+        .order_by(Post.id.desc())
+        .limit(10)
+        .all()
+    ):
+        post_list.append(post.id)
+
+    print(post_list)
