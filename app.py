55d85b46753c5feaad0d0993c183c39eaa402f48
Alexander S <alexsvistunov73@gmail.com>
2023-12-21 14:51:30 +0300

    rewrite endpoints with using sqlalchemy

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
 import datetime
 from fastapi import Depends, FastAPI, HTTPException
-import psycopg2
-from psycopg2.extras import RealDictCursor
+from sqlalchemy.orm import Session
+from typing import List
+
+from database import SessionLocal
+from table_post import Post
+from table_user import User
 from schema import UserGet, PostGet
 
 def get_db():
-    connection = psycopg2.connect(
-        database='startml',
-        host='postgres.lab.karpov.courses',
-        user='robot-startml-ro',
-        password='pheiph0hahj1Vaif',
-        port=6432,
-        cursor_factory=RealDictCursor
-    )
-
-    return connection
+    with SessionLocal() as db:
+        return db
 
 app = FastAPI()
 
@@ -22,32 +18,27 @@ app = FastAPI()
 def root() -> str:
     return 'root'
 
-@app.get('/sum_date')
-def sum_date(current_date: datetime.date, offset: int):
-    return current_date + datetime.timedelta(days=offset)
-
-@app.post('/user/validate')
-def validate(user: UserGet):
-    return f'Will add user: {user.name} {user.surname} with age {user.age}'
+@app.get('/user/all', response_model=List[UserGet])
+def get_all_users(limit: int = 10, db: Session = Depends(get_db)):
+    return db.query(User).limit(limit).all()
 
 @app.get('/user/{id}', response_model=UserGet)
-def user(id: int, db=Depends(get_db)):
-    with db.cursor() as cursor:
-        cursor.execute("""SELECT * FROM "user" WHERE id='%s'""" % id)
-        results = cursor.fetchone()
+def user(id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == int(id)).all()
 
-        if not results:
-            raise HTTPException(404, detail='user not found')
+    if not user:
+        raise HTTPException(404, detail='user not found')
 
-        return UserGet(**results)
+    return user[0]
 
+@app.get('/post/all', response_model=List[PostGet])
+def get_all_users(limit: int = 10, db: Session = Depends(get_db)):
+    return db.query(Post).limit(limit).all()
 @app.get('/post/{id}', response_model=PostGet)
-def user(id: int, db=Depends(get_db)):
-    with db.cursor() as cursor:
-        cursor.execute("""SELECT id, text, topic FROM "post" WHERE id='%s'""" % id)
-        results = cursor.fetchone()
+def user(id: int, db: Session=Depends(get_db)):
+    post = db.query(Post.id, Post.text, Post.topic).filter(Post.id == int(id)).all()
 
-        if not results:
-            raise HTTPException(404, detail='post not found')
+    if not post:
+        raise HTTPException(404, detail='post not found')
 
-        return PostGet(**results)
+    return post[0]
