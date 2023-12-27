41ff2692a3468f3cacf3db3bd311a8aae597b3c5
Alexander S <alexsvistunov73@gmail.com>
2023-12-27 13:33:16 +0300

    hide secrets using dotenv

--- database.py
+++ database.py
@@ -1,8 +1,19 @@
-from sqlalchemy import create_engine
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, URL
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://robot-startml-ro:pheiph0hahj1Vaif@postgres.lab.karpov.courses:6432/startml"
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = URL.create(
+    "postgresql",
+    username=os.environ["POSTGRES_USER"],
+    password=os.environ["POSTGRES_PASSWORD"],
+    host=os.environ["POSTGRES_HOST"],
+    port=int(os.environ["POSTGRES_PORT"]),
+    database=os.environ["POSTGRES_DATABASE"],
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
