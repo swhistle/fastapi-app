88a1244862530fef2e357d1b1ac1528a5f623212
Alexander S <alexsvistunov73@gmail.com>
2024-05-15 12:59:01 +0300

    add uploading processed data to remote db

---
+++ internals/scripts/data_preprocessing/data_upload_to_db.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+from internals.db_connection.connection import connection_url
+
+engine = create_engine(connection_url)
+
+df_posts_features = pd.read_csv('internals/data/data_posts_features.csv', sep=';')
+df_posts_features.to_sql('al-svistunov_posts_features_lesson_22', con=engine)
+
+df_users_features = pd.read_csv('internals/data/data_users_features.csv', sep=';')
+df_users_features.to_sql('al-svistunov_users_features_lesson_22', con=engine)
