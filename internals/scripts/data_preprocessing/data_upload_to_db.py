55d461912ba348200f61ebae97e77d19fe50c1a8
Alexander S <alexsvistunov73@gmail.com>
2024-05-24 11:23:07 +0300

    fix data preprocessing, drop duplicates

--- internals/scripts/data_preprocessing/data_upload_to_db.py
+++ internals/scripts/data_preprocessing/data_upload_to_db.py
@@ -6,7 +6,7 @@ from internals.db_connection.connection import connection_url
 engine = create_engine(connection_url)
 
 df_posts_features = pd.read_csv('internals/data/data_posts_features.csv', sep=';')
-df_posts_features.to_sql('al-svistunov_posts_features_lesson_22', con=engine)
+df_posts_features.to_sql('al-svistunov_posts_features_lesson_22', con=engine, if_exists="replace")
 
 df_users_features = pd.read_csv('internals/data/data_users_features.csv', sep=';')
-df_users_features.to_sql('al-svistunov_users_features_lesson_22', con=engine)
+df_users_features.to_sql('al-svistunov_users_features_lesson_22', con=engine, if_exists="replace")
